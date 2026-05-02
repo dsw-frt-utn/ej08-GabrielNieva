0b92c9a8dcc19b8e05fa0a2e84b2886b9ff7cf3e
GabrielNieva <gabrielagustinnieva@gmail.com>
2026-05-02 17:14:06 -0300

    Problema 2 resuelto y saque las clases de dentro de las carpetas

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -11,6 +11,15 @@
             var helper = new ProductHelper();
             string etiqueta = helper.ObtenerEtiquetaProducto(12345, "Camiseta", 19.99m);
             Console.WriteLine("Problema 1: " + etiqueta);
+
+            //Problema 2
+
+            var p2 = new Problema2();
+            string resumen = p2.CrearResumenVenta(202, "Pepsi", 3, 500m);
+            Console.WriteLine("Problema 2: " + resumen);
+
+            string resumen2 = p2.CrearResumenVenta(303, "Coca-Cola", 0, 450m);
+            Console.WriteLine("Problema 2 (con cantidad 0): " + resumen2);
         }
     }
 }
