04d3792e8fc5f09f444d08ba20ef5b4f6cf69fa3
GabrielNieva <gabrielagustinnieva@gmail.com>
2026-05-02 16:44:03 -0300

    Problema 1 resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Problema 1
+
+            var helper = new ProductHelper();
+            string etiqueta = helper.ObtenerEtiquetaProducto(12345, "Camiseta", 19.99m);
+            Console.WriteLine("Problema 1: " + etiqueta);
         }
     }
 }
